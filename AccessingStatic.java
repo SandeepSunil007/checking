4258ac9e00b48f89af4b19c55ecc57fb66be3fbb
TY_SunilKumar <115223014+SandeepSunil007@users.noreply.github.com>
2023-02-23 15:53:20 +0530

    Delete AccessingStatic.java

--- AccessingStatic.java
+++
@@ -1,8 +0,0 @@
-class AccessingStatic{
-
-   public static void main(String[] args){
-      System.out.println("Accessing the static variable : "+Static_NormalVAriables.a);
-       Static_NormalVAriables  acc = new Static_NormalVAriables ();
-     System.out.println("normal variable calling using obj refence : "+acc.name);
-}
-}
